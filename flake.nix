0cafabfda7b195c1d3e8cddfb7fbfd1214ac295f
Aaron Gorodetzky <aaron@nytework.com>
2024-03-21 12:27:26 -0400

    Dev changes

--- flake.nix
+++ flake.nix
@@ -20,6 +20,30 @@
       ];
       username = "aargonian";
       hostname = "NixosPersonal";
+
+      # Primarily used for the develop shell for tauri
+      libraries = with pkgs; [
+        webkitgtk
+        gtk3
+        cairo
+        gdk-pixbuf
+        glib
+        dbus
+        openssl_3
+        librsvg
+      ];
+      packages = with pkgs; [
+        curl
+        wget
+        pkg-config
+        dbus
+        openssl_3
+        glib
+        gtk3
+        libsoup
+        webkitgtk
+        librsvg
+      ];
     in {
 
     nixosConfigurations = {
@@ -47,6 +71,13 @@
           inherit pkgs-unstable;
         };
       };
+    };
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = packages;
+      shellHook = ''
+          export LD_LIBRARY_PATH=${lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+          export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+      '';
     };
   };
 }

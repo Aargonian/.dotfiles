1ab14f3e93adeae58bcd0347718f7da60371c890
Aaron Gorodetzky <aaron@nytework.com>
2024-06-27 00:04:22 -0400

    Enable DisplayLink

--- system/package_sets/desktop_environment.nix
+++ system/package_sets/desktop_environment.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}:
+{pkgs, lib, ...}:
 {
   # Enable the X11 windowing system.
   services.xserver = {
@@ -61,6 +61,15 @@
 
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
+
+  # DisplayLink Driver Support
+  services.xserver.videoDrivers = [ "displaylink" "modesetting" ];
+  services.xserver.displayManager.sessionCommands = ''
+      ${lib.getBin pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource 2 0
+  '';
+
+  # Note: The following command may need to be ran before the DL driver will work:
+  # nix-prefetch-url --name displaylink-580.zip https://www.synaptics.com/sites/default/files/exe_files/2023-08/DisplayLink%20USB%20Graphics%20Software%20for%20Ubuntu5.8-EXE.zip
 
   # Various useful graphics programs
   environment.systemPackages = with pkgs; [

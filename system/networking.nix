fef07b3920a6947c2081ac59fba7ffa3324711bc
Aaron Gorodetzky <aaron@nytework.com>
2024-08-20 18:32:16 -0400

    Fix networking DNS issues when not connected to Mullvad VPN

--- system/networking.nix
+++ system/networking.nix
@@ -1,9 +1,14 @@
-{ hostname, ... }:
+{ pkgs, hostname, ... }:
 {
   networking.hostName = hostname; # Define your hostname.
   networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   networking.firewall.allowedTCPPorts = [ 22 ];
   time.timeZone = "America/New_York";
+
+  # Useful networking tools
+  environment.systemPackages = with pkgs; [
+    dig
+  ];
 
   # Setup Mullvad VPN
   services.mullvad-vpn.enable = true;
@@ -11,7 +16,7 @@
   # Mullvad currently requires systemd-resolved
   services.resolved = {
     enable = true;
-    dnssec = "true";
-    dnsovertls = "true";
+    dnssec = "false";
+    dnsovertls = "false";
   };
 }

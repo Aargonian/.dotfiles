2c656cf53e56d58a142a74842cc6c2ef1d3aaa51
Aaron Gorodetzky <aaron@nytework.com>
2024-08-29 20:49:59 -0400

    Some more nice packages

--- user/packages.nix
+++ user/packages.nix
@@ -34,10 +34,37 @@
     epubcheck
     transmission-qt
     libreoffice
+
+    # Jetbrains IDEs
+    jetbrains.rust-rover
+    jetbrains.webstorm
+    jetbrains.writerside
+    jetbrains.pycharm-community-bin
+    jetbrains.clion
+
+    # Bitwarden and Qutebrowser
+    bitwarden-cli
+
+    # To use qute-bitwarden
+    keyutils
+    rofi
   ];
 
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.permittedInsecurePackages = [
     "electron-25.9.0"
   ];
+
+  programs.qutebrowser = {
+    enable = true;
+#    greasemonkey = [
+#      (pkgs.fetchurl {
+#        url = "https://raw.githubusercontent.com/qutebrowser/qutebrowser/8ddaef35d0c05e226a9018647a2f2874456435f9/misc/userscripts/qute-bitwarden";
+#        sha256 = "1j84gbyzbzhyn5xn5h4d2mx01fynvhyvp3lfmsp7f29fiw8zrv30";
+#      })
+#    ];
+    searchEngines = {
+      DEFAULT = "https://www.google.com/search?hl=en&q={}";
+    };
+  };
 }

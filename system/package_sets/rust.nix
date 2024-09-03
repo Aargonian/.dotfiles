3b5924c54a177b8c610599ace112538a323ce241
Aaron Gorodetzky <aaron@nytework.com>
2024-09-03 15:05:25 -0400

    Remove rust rover from rust packages

--- system/package_sets/rust.nix
+++ system/package_sets/rust.nix
@@ -1,18 +1,10 @@
 { pkgs, pkgs-unstable, ... }:
 {
-  environment.systemPackages =
-    (with pkgs; [
-      rustup
-      ripgrep
-      mprocs
-      du-dust
-      zoxide
-      pkgs-unstable.jetbrains.rust-rover
-    ])
-
-    ++
-
-    (with pkgs-unstable; [
-      jetbrains.rust-rover
-    ]);
+  environment.systemPackages = with pkgs; [
+    rustup
+    ripgrep
+    mprocs
+    du-dust
+    zoxide
+  ];
 }

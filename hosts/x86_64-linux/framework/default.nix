b74fe5d63a17b449abdc877a5bf86b4dc4e06407
Aaron Gorodetzky <aaron@nytework.com>
2024-09-17 14:44:55 -0400

    Remove unnecessary options

--- hosts/x86_64-linux/framework/default.nix
+++ hosts/x86_64-linux/framework/default.nix
@@ -80,7 +80,6 @@
         fileSystems."/run/media/aargonian/InternalData" = {
           device = "/dev/disk/by-uuid/7b75839e-56c3-4e31-8d4e-a69a61cdc653";
           fsType = "btrfs";
-          options = [ "rw" ];
         };
 
         swapDevices = [ ];

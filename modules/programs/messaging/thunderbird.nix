a9aad89c8b8e0997083a7503fa053843cbc90cb4
Aaron Gorodetzky <aaron@nytework.com>
2024-10-11 12:20:39 -0400

    Fix thunderbird

--- modules/programs/messaging/thunderbird.nix
+++ modules/programs/messaging/thunderbird.nix
@@ -4,13 +4,9 @@
     enable = mkEnableOption "Thunderbird Email Client";
   };
 
-  #onfig = mkIf config.custom.programs.thunderbird.enable {
-  # home-manager.users.${config.custom.username} = {
-  #   programs.thunderbird = {
-  #     enable = true;
-  #   };
-  # };
-
-  # # TODO: Use home manager to set up thunderbird profile after adjusting settings where I want!
-  #;
+  config = mkIf config.custom.programs.thunderbird.enable {
+    environment.systemPackages = with pkgs; [
+      thunderbird
+    ];
+  };
 }

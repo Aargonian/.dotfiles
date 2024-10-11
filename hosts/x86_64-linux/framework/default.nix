4dbf05e44ff27f73886c378b8d5c3f0e00482f74
Aaron Gorodetzky <aaron@nytework.com>
2024-10-11 12:52:19 -0400

    Move configuration into variable

--- hosts/x86_64-linux/framework/default.nix
+++ hosts/x86_64-linux/framework/default.nix
@@ -2,6 +2,134 @@
 let
   username = "aargonian";
   hostname = "NytegearFramework";
+
+  configuration = { lib, config, pkgs, options, ... }: {
+    hardware.enableRedistributableFirmware = lib.mkDefault true;
+    users.aargonian.enable = true;
+
+    custom = {
+      username = username;
+
+      system = {
+        audio.enable = true;
+        bluetooth.enable = true;
+        networking = {
+          enable = true;
+          hostname = hostname;
+          vpn.enable = true;
+        };
+        display.enable = true;
+
+        display.desktopManagers.cinnamon.enable = true;
+      };
+
+      services = {
+        greetd.enable = true;
+      };
+
+      programs = {
+        # Package Sets
+        audio.all = true;
+        development.all = true;
+        messaging.all = true;
+        other.all = true;
+        productivity.all = true;
+        security.all = true;
+        shell.all = true;
+        utility.all = true;
+
+        # Individual
+        firefox.enable = true;
+        steam.enable = true;
+        xfce4-terminal.enable = true;
+      };
+    };
+
+    services.power-profiles-daemon.enable = true;
+
+    # Install virtualbox
+    virtualisation.virtualbox.host.enable = true;
+    users.extraGroups.vboxusers.members = [
+      username
+    ];
+
+    environment.systemPackages = with pkgs; [
+      tigervnc
+      liferea
+      gsmartcontrol
+
+      # AMD GPU Control
+      lact
+    ];
+
+    fileSystems = {
+      "/" = {
+        device = "/dev/disk/by-uuid/9e412de9-bef7-4747-9116-c01582fb22c1";
+        fsType = "btrfs";
+      };
+
+      "/boot" = {
+        device = "/dev/disk/by-uuid/1FA3-F2D7";
+        fsType = "vfat";
+        options = [ "fmask=0022" "dmask=0022" ];
+      };
+
+      # Mount the big data partition
+      "/run/media/aargonian/InternalData" = {
+        device = "/dev/disk/by-uuid/7b75839e-56c3-4e31-8d4e-a69a61cdc653";
+        fsType = "btrfs";
+      };
+
+      # Mount Framework Portable SSD if Present (Usually in the left slot)
+      "/run/media/aargonian/FrameworkPortable" = {
+        device = "/dev/disk/by-uuid/203ECDA4274108EA";
+        fsType = "ntfs";
+        options = [ "nofail" "fmask=011" "dmask=000" ];
+      };
+
+      # Mount Temporary Framework SSD
+      "/run/media/aargonian/FrameworkAuxillary" = {
+        device = "/dev/disk/by-uuid/a0561ab4-8f9d-41f2-bdfc-e357660b8307";
+        fsType = "btrfs";
+        options = [ "nofail" ];
+      };
+    };
+
+    swapDevices = [ ];
+
+    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" ];
+    boot.initrd.kernelModules = [ "amdgpu" ];
+    boot.kernelModules = [ "kvm-amd" ];
+    boot.extraModulePackages = [ ];
+
+    # Mediatek Driver fix for Framework
+    hardware.wirelessRegulatoryDatabase = true;
+    boot.extraModprobeConfig = ''
+      options cfg80211 ieee80211_regdom="US"
+    '';
+
+    #nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.amd.updateMicrocode = lib.mkDefault true;
+
+    # Graphic Stuff
+    hardware.opengl = {
+      extraPackages = with pkgs; [ amdvlk ];
+      extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+
+    # Control AMD GPU with Lact
+    systemd.packages = with pkgs; [ lact ];
+    systemd.services.lactd.wantedBy = [ "multi-user.target" ];
+
+    # Add Raspberry Pi Server to Hosts
+    networking.extraHosts = ''
+      192.168.50.7 rpi
+      192.168.50.1 router
+    '';
+  };
 in
 {
   framework = lib.nixosSystem {
@@ -27,134 +155,7 @@ in
       }
       inputs.nixos-hardware.nixosModules.framework-16-7040-amd
 
-      ( { lib, config, pkgs, options, ... }: {
-          hardware.enableRedistributableFirmware = lib.mkDefault true;
-          users.aargonian.enable = true;
-
-          custom = {
-            username = username;
-
-            system = {
-              audio.enable = true;
-              bluetooth.enable = true;
-              networking = {
-                enable = true;
-                hostname = hostname;
-                vpn.enable = true;
-              };
-              display.enable = true;
-
-              display.desktopManagers.cinnamon.enable = true;
-            };
-
-            services = {
-              greetd.enable = true;
-            };
-
-            programs = {
-              # Package Sets
-              audio.all = true;
-              development.all = true;
-              messaging.all = true;
-              other.all = true;
-              productivity.all = true;
-              security.all = true;
-              shell.all = true;
-              utility.all = true;
-
-              # Individual
-              firefox.enable = true;
-              steam.enable = true;
-              xfce4-terminal.enable = true;
-            };
-          };
-
-          services.power-profiles-daemon.enable = true;
-
-          # Install virtualbox
-          virtualisation.virtualbox.host.enable = true;
-          users.extraGroups.vboxusers.members = [
-            username
-          ];
-
-          environment.systemPackages = with pkgs; [
-            tigervnc
-            liferea
-            gsmartcontrol
-
-            # AMD GPU Control
-            lact
-          ];
-
-          fileSystems = {
-            "/" = {
-              device = "/dev/disk/by-uuid/9e412de9-bef7-4747-9116-c01582fb22c1";
-              fsType = "btrfs";
-            };
-
-            "/boot" = {
-              device = "/dev/disk/by-uuid/1FA3-F2D7";
-              fsType = "vfat";
-              options = [ "fmask=0022" "dmask=0022" ];
-            };
-
-            # Mount the big data partition
-            "/run/media/aargonian/InternalData" = {
-              device = "/dev/disk/by-uuid/7b75839e-56c3-4e31-8d4e-a69a61cdc653";
-              fsType = "btrfs";
-            };
-
-            # Mount Framework Portable SSD if Present (Usually in the left slot)
-            "/run/media/aargonian/FrameworkPortable" = {
-              device = "/dev/disk/by-uuid/203ECDA4274108EA";
-              fsType = "ntfs";
-              options = [ "nofail" "fmask=011" "dmask=000" ];
-            };
-
-            # Mount Temporary Framework SSD
-            "/run/media/aargonian/FrameworkAuxillary" = {
-              device = "/dev/disk/by-uuid/a0561ab4-8f9d-41f2-bdfc-e357660b8307";
-              fsType = "btrfs";
-              options = [ "nofail" ];
-            };
-          };
-
-          swapDevices = [ ];
-
-          boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" ];
-          boot.initrd.kernelModules = [ "amdgpu" ];
-          boot.kernelModules = [ "kvm-amd" ];
-          boot.extraModulePackages = [ ];
-
-          # Mediatek Driver fix for Framework
-          hardware.wirelessRegulatoryDatabase = true;
-          boot.extraModprobeConfig = ''
-            options cfg80211 ieee80211_regdom="US"
-          '';
-
-          #nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-          hardware.cpu.amd.updateMicrocode = lib.mkDefault true;
-
-          # Graphic Stuff
-          hardware.opengl = {
-            extraPackages = with pkgs; [ amdvlk ];
-            extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
-            enable = true;
-            driSupport = true;
-            driSupport32Bit = true;
-          };
-
-          # Control AMD GPU with Lact
-          systemd.packages = with pkgs; [ lact ];
-          systemd.services.lactd.wantedBy = [ "multi-user.target" ];
-
-          # Add Raspberry Pi Server to Hosts
-          networking.extraHosts = ''
-            192.168.50.7 rpi
-            192.168.50.1 router
-          '';
-        }
-      )
+      configuration
     ];
   };
 }

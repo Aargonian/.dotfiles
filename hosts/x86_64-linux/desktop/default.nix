5a38266bb6d1db1116672ae0b641b87846c7a54b
Aaron Gorodetzky <aaron@nytework.com>
2024-10-11 13:24:09 -0400

    Remove boilerplate

--- hosts/x86_64-linux/desktop/default.nix
+++ hosts/x86_64-linux/desktop/default.nix
@@ -60,53 +60,10 @@ let
         fsType = "vfat";
         options = [ "fmask=0022" "dmask=0022" ];
       };
-
-      # Mount the big data partition
-      "/run/media/aargonian/InternalData" = {
-        device = "/dev/disk/by-uuid/7b75839e-56c3-4e31-8d4e-a69a61cdc653";
-        fsType = "btrfs";
-      };
-
-      # Mount Framework Portable SSD if Present (Usually in the left slot)
-      "/run/media/aargonian/FrameworkPortable" = {
-        device = "/dev/disk/by-uuid/203ECDA4274108EA";
-        fsType = "ntfs";
-        options = [ "nofail" "fmask=011" "dmask=000" ];
-      };
-
-      # Mount Temporary Framework SSD
-      "/run/media/aargonian/FrameworkAuxillary" = {
-        device = "/dev/disk/by-uuid/a0561ab4-8f9d-41f2-bdfc-e357660b8307";
-        fsType = "btrfs";
-        options = [ "nofail" ];
-      };
     };
 
     swapDevices = [ ];
-
-    boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usb_storage" "usbhid" "sd_mod" ];
-    boot.initrd.kernelModules = [ "amdgpu" ];
-    boot.kernelModules = [ "kvm-amd" ];
-    boot.extraModulePackages = [ ];
-
-    # Mediatek Driver fix for Framework
-    hardware.wirelessRegulatoryDatabase = true;
-    boot.extraModprobeConfig = ''
-      options cfg80211 ieee80211_regdom="US"
-    '';
-
     nixpkgs.hostPlatform = system-name;
-    hardware.cpu.amd.updateMicrocode = true;
-    hardware.enableRedistributableFirmware = true;
-
-    # Graphic Stuff
-    hardware.opengl = {
-      extraPackages = with pkgs; [ amdvlk ];
-      extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-    };
 
     # Add Raspberry Pi Server to Hosts
     networking.extraHosts = ''
@@ -137,7 +94,6 @@ in
           users-path
         ];
       }
-      inputs.nixos-hardware.nixosModules.framework-16-7040-amd
 
       configuration
     ];

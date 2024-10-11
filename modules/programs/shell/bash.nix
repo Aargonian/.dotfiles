3c11da1e2b7309cc6a951a7cfbc4d0996bb417c3
Aaron Gorodetzky <aaron@nytework.com>
2024-10-11 12:39:02 -0400

    Fix bash install

--- modules/programs/shell/bash.nix
+++ modules/programs/shell/bash.nix
@@ -1,9 +1,10 @@
-{ lib, config, ... }: with lib;
+{ lib, config, pkgs, ... }: with lib;
 {
   options.custom.programs.bash.enable = mkEnableOption "Bash Shell Support";
 
   config = mkIf config.custom.programs.zsh.enable {
-# TODO: Apparently this has no effect. Look into this.
-#   programs.bash.enable = true;
+    environment.systemPackages = with pkgs; [
+      bash
+    ];
   };
 }

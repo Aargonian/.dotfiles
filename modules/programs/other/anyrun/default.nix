6b97c8f5c779488a2aa71e21566d0ffe9db91d55
Aaron Gorodetzky <aaron@nytework.com>
2024-10-11 12:02:32 -0400

    Fix Anyrun

--- modules/programs/other/anyrun/default.nix
+++ modules/programs/other/anyrun/default.nix
@@ -1,37 +1,46 @@
 { lib, config, pkgs, inputs, ... }: with lib;
 {
-# TODO: Anyrun appears to be borked in new config. Figure out why.
-# imports = [
-#   inputs.anyrun.homeManagerModules.default
-# ];
-
   options.custom.programs.anyrun.enable = mkEnableOption "Enable anyrun program launcher";
 
-# config = mkIf config.custom.programs.anyrun.enable {
-#   home-manager.users.${config.custom.username} = {
-#     programs.anyrun = {
-#       enable = true;
-#       config = {
-#         plugins = with inputs.anyrun.packages.${pkgs.system}; [
-#           applications
-#           rink
-#           # randr
-#           shell
-#           kidex
-#           symbols
-#         ];
+  config = mkIf config.custom.programs.anyrun.enable {
+    # Use binary cache for anyrun if possible
+    nix.settings = {
+      builders-use-substitutes = true;
+      extra-substituters = [
+        "https://anyrun.cachix.org"
+      ];
+      extra-trusted-public-keys = [
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+      ];
+    };
 
-#         width.fraction = 0.25;
-#         y.fraction = 0.3;
-#         hidePluginInfo = true;
-#         closeOnClick = true;
-#         hideIcons = false;
-#         showResultsImmediately = true;
-#         maxEntries = null;
-#       };
+    home-manager.users.${config.custom.username} = {
+      imports = [
+        inputs.anyrun.homeManagerModules.default
+      ];
+      programs.anyrun = {
+        enable = true;
+        config = {
+          plugins = with inputs.anyrun.packages.${pkgs.system}; [
+            applications
+            rink
+            # randr
+            shell
+            kidex
+            symbols
+          ];
 
-#       extraCss = builtins.readFile(./. + "/anyrun-style.css");
-#     };
-#   };
-# };
+          width.fraction = 0.25;
+          y.fraction = 0.3;
+          hidePluginInfo = true;
+          closeOnClick = true;
+          hideIcons = false;
+          showResultsImmediately = true;
+          maxEntries = null;
+        };
+
+        extraCss = builtins.readFile(./. + "/anyrun-style.css");
+      };
+    };
+  };
 }

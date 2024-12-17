aa8017a3a9f5f8e5636481b005a02bae6df209c9
Aaron Gorodetzky <aaron@nytework.com>
2024-12-16 22:26:36 -0500

    Clarify path purposes

--- users/default.nix
+++ users/default.nix
@@ -10,57 +10,57 @@
 
     localData = lib.mkOption {
       type = lib.types.path;
-      description = "Path where data is stored in the home directory";
+      description = "Path where local machine data is stored";
     };
 
     appData = lib.mkOption {
       type = lib.types.path;
-      description = "Path where data is stored in the home directory";
+      description = "Path where application data is stored";
     };
 
     scripts = lib.mkOption {
       type = lib.types.path;
-      description = "Path where data is stored in the home directory";
+      description = "Path where scripts/programs are stored";
     };
 
     configs = lib.mkOption {
       type = lib.types.path;
-      description = "Path where data is stored in the home directory";
+      description = "Path where application configs are stored";
     };
 
     cache = lib.mkOption {
       type = lib.types.path;
-      description = "Path where data is stored in the home directory";
+      description = "Path where application cache data is stored";
     };
 
     dataDirRelative = lib.mkOption {
       type = lib.types.path;
-      description = "Path where data is stored in the home directory";
+      description = "Relative (to home) path where data is stored";
     };
 
     localDataRelative = lib.mkOption {
       type = lib.types.path;
-      description = "Path where data is stored in the home directory";
+      description = "Relative (to home) path where local machine data is stored";
     };
 
     appDataRelative = lib.mkOption {
       type = lib.types.path;
-      description = "Path where data is stored in the home directory";
+      description = "Relative (to home) path where applicatino data is stored";
     };
 
     scriptsRelative = lib.mkOption {
       type = lib.types.path;
-      description = "Path where data is stored in the home directory";
+      description = "Relative (to home) path where scripts/programs are stored";
     };
 
     configsRelative = lib.mkOption {
       type = lib.types.path;
-      description = "Path where data is stored in the home directory";
+      description = "Relative (to home) path where config data is stored";
     };
 
     cacheRelative = lib.mkOption {
       type = lib.types.path;
-      description = "Path where data is stored in the home directory";
+      description = "Relative (to home) path where cache data is stored";
     };
   };
 

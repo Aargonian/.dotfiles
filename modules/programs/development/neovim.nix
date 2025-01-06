55fcc9c7d121ef246d337e30dbdf48e90002468c
Aaron Gorodetzky <aaron@nytework.com>
2025-01-06 16:19:53 -0500

    Use unstable neovim

--- modules/programs/development/neovim.nix
+++ modules/programs/development/neovim.nix
@@ -1,16 +1,17 @@
-{ lib, config, pkgs, ... }: with lib;
+{ lib, config, pkgs, pkgs-unstable, ... }: with lib;
 {
   options.custom.programs.neovim = {
     enable = mkEnableOption "Shell programs";
   };
 
   config = mkIf config.custom.programs.neovim.enable {
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-    };
+#   programs.neovim = {
+#     enable = true;
+#     defaultEditor = true;
+#   };
 
     environment.systemPackages = with pkgs; [
+      pkgs-unstable.neovim
       ripgrep
       fzf # For neovim telescope plugin
       xclip # Neovim clipboard provider

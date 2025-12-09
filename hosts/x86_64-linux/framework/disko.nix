61f66b441016b7bfb775f10e20de7b8f94d2e842
Aaron Gorodetzky <aaron@nytegear.com>
2025-12-09 10:59:47 -0500

    Initial disko setup

---
+++ hosts/x86_64-linux/framework/disko.nix
@@ -0,0 +1,33 @@
+{ disks ? [ "/dev/nvme1n1" ], ... }: {
+  disko.devices = {
+    disk = {
+      my-disk = {
+        device = builtins.elemAt disks 0;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}

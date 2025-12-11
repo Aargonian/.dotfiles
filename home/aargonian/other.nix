850935dd4f10857daa9af23c990e12b7898b82dd
Aaron Gorodetzky <aaron@nytework.com>
2025-12-11 02:48:19 -0500

    Make todo wrapper

--- home/aargonian/other.nix
+++ home/aargonian/other.nix
@@ -1,6 +1,13 @@
 { pkgs, ... }:
+let
+  todoWrapper = pkgs.writeShellScriptBin "todo" ''
+    #!${pkgs.zsh}/bin/zsh
+    todoist quick "$*"
+  '';
+in
 {
   home.packages = with pkgs; [
     todoist
+    todoWrapper
   ];
 }
